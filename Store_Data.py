c34b8188fb65f9774664a4bc51bd340a5f9a8701
wonderspace37 <136860951+wonderspace37@users.noreply.github.com>
2025-03-01 15:11:28 -0800

    Add files via upload

--- Store_Data.py
+++ Store_Data.py
@@ -1,5 +1,12 @@
+import os
+from machine import Pin, SoftSPI
+from sdcard import SDCard
+from imu_libs.mpu9250 import MPU9250
+import numpy as np
+
+
 def Store_Raw_Data():
-    x = x
+    x = Pin, SoftSPI
 
 
 def Store_Data_Filtered():
