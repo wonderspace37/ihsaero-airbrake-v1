9a5b40d6647397244db6dc9d175ab1e2099a4696
wonderspace37 <136860951+wonderspace37@users.noreply.github.com>
2025-03-01 15:13:58 -0800

    Add files via upload

--- Store_Data.py
+++ Store_Data.py
@@ -4,6 +4,18 @@ from sdcard import SDCard
 from imu_libs.mpu9250 import MPU9250
 import numpy as np
 
+acc_Raw = np.array([0, 0, 0])
+gyro_Raw = np.array([0, 0, 0])
+mag_Raw = np.array([0, 0, 0, 0, 0])
+temp_Raw = 0
+
+acc_Filtered = np.array([0, 0, 0])
+gyro_Filtered = np.array([0, 0, 0])
+mag_Filtered = np.array([0, 0, 0])
+temp_Filtered = 0
+
+def readData():
+    
 
 def Store_Raw_Data():
     x = Pin, SoftSPI
